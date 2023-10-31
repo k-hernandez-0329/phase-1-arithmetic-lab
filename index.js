c800a0788645308b05b97c760b5a92dea04cd7db
karenhernandez <khernandez0329@gmail.com>
2023-10-30 18:27:33 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1 = 31
+const num2 = 2
+
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 5) + 0
+
+
+const num3 = 20
+const num4= 5
+const mod = num3 / num4
+
+
+const max = 20
